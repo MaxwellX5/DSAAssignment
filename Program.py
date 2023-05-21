498f4dd486e277c467d0f5227214d745b709484e
MaxwellX5 <matthew.oliver.wong@gmail.com>
2023-05-21 17:38:02 +0800

    bruh

--- Program.py
+++ Program.py
@@ -1,16 +1,16 @@
 from Student import Student
 students = []
-s1 = Student(1,"bob","lol",1111,"IT2021")
+s1 = Student("220261J","bob","lol",1111,"IT2021")
 
-s2 = Student(4,"John","lol",1111,"DSF2001")
-s3 = Student(2,"Don","lol",1111,"IT2001")
-s4 = Student(9,"Don3","lol",1111,"DAAA2001")
-s5 = Student(7,"Bruh","lol",1111,"DSF2021")
-s6 = Student(10,"urmom","lol",1111,"CIP2001")
-s7 = Student(5,"Bong","lol",1111,"DAAA2021")
-s8 = Student(8,"Mah","lol",1111,"CIP2021")
-s9 = Student(3,"Fred","lol",1111,"DBFT2001")
-s10 = Student(6,"Luck","lol",1111,"DBFT2021")
+s2 = Student("221261J","John","lol",1111,"DSF2001")
+s3 = Student("223261J","Don","lol",1111,"IT2001")
+s4 = Student("224261J","Don3","lol",1111,"DAAA2001")
+s5 = Student("225261J","Bruh","lol",1111,"DSF2021")
+s6 = Student("261261J","urmom","lol",1111,"CIP2001")
+s7 = Student("227261J","zzzg","lol",1111,"DAAA2021")
+s8 = Student("229261J","aaaaaaaaaa","lol",1111,"CIP2021")
+s9 = Student("231261J","Fred","lol",1111,"DBFT2001")
+s10 = Student("201261J","Luck","lol",1111,"DBFT2021")
 students.append(s1)
 students.append(s2)
 students.append(s3)
@@ -28,10 +28,46 @@ def display_students(students):
 
 def add_student(students):
     admin_no = input("Enter AdminNo: ")
+    duplicate = False
+    for student in students:
+        if admin_no == student.get_admin_no():
+            duplicate_found = True
+    while admin_no.strip() == "" or admin_no[-1].isalpha() == False or admin_no[0:-1].isdigit() == False or len(admin_no) != 7 or duplicate_found == True:
+        if duplicate_found == True:
+            print("AdminNo already exists")
+        else:
+            print("Invalid AdminNo")
+        admin_no = input("Enter AdminNo: ")
+        # Check for duplicate AdminNo
+        duplicate_found = False
+        for student in students:
+            if admin_no == student.get_admin_no():
+                duplicate_found = True
+                break
+        if duplicate_found:
+            continue
+        # If AdminNo is valid and not a duplicate, proceed
+
+
     name = input("Enter Student Name: ")
+    while name.strip() == "" and name.isalpha() == False:
+        print("Invalid name")
+        name = input("Enter Student Name:")
     email = input("Enter Student Email: ")
-    year_admitted = int(input("Enter Year Admitted: "))
+    while "@" not in email or "." not in email:
+        print("Invalid email. Student Email should be a valid email address.")
+        email = input("Enter Student Email: ")
+
+    year_admitted = (input("Enter Year Admitted: "))
+    while not year_admitted.isdigit():
+        print("Invalid year. Year Admitted should be a numeric value.")
+        year_admitted = input("Enter Year Admitted: ")
+    year_admitted = int(year_admitted)
+
     pem_group = input("Enter PEM Group: ")
+    while pem_group.isalnum() == False:
+        print("Invalid PEM Group. PEM Group should only contain alphanumeric characters.")
+        pem_group = input("Enter PEM Group: ")
     student = Student(admin_no,name,email,year_admitted,pem_group)
     students.append(student)
 
@@ -61,6 +97,8 @@ def insertion_sort_pem_group(students):
 
 def main():
     while True:
+        print("")
+        print("*******************************")
         print("Student Management System Menu:")
         print("1. Display all students.")
         print("2. Add a new student.")
