e2dbb142ba82dac8e0b8e6401445a91c18d63441
MaxwellX5 <220261J@mymail.nyp.edu.sg>
2023-05-31 11:15:21 +0800

    bruh

--- Program.py
+++ Program.py
@@ -1,30 +1,12 @@
 from Student import Student
-students = []
-s1 = Student("220261J","bob","lol",1111,"IT2021")
-
-s2 = Student("221261J","John","lol",1111,"DSF2001")
-s3 = Student("223261J","Don","lol",1111,"IT2001")
-s4 = Student("224261J","Don3","lol",1111,"DAAA2001")
-s5 = Student("225261J","Bruh","lol",1111,"DSF2021")
-s6 = Student("261261J","urmom","lol",1111,"CIP2001")
-s7 = Student("227261J","zzzg","lol",1111,"DAAA2021")
-s8 = Student("229261J","aaaaaaaaaa","lol",1111,"CIP2021")
-s9 = Student("231261J","Fred","lol",1111,"DBFT2001")
-s10 = Student("201261J","Luck","lol",1111,"DBFT2021")
-students.append(s1)
-students.append(s2)
-students.append(s3)
-students.append(s4)
-students.append(s5)
-students.append(s6)
-students.append(s7)
-students.append(s8)
-students.append(s9)
-students.append(s10)
 
 def display_students(students):
-    for student in students:
-        print(student)
+    if len(students) == 0:
+        print("No students entered yet")
+
+    else:
+        for student in students:
+            print(student)
 
 def add_student(students):
     admin_no = input("Enter AdminNo: ")
@@ -73,17 +55,34 @@ def add_student(students):
 
 def bubble_sort_admin_no(students):
     n = len(students)
+    count = 1
+
     for i in range(n-1,0,-1):
+        swapmade = False
         for j in range(i):
             if students[j].get_admin_no() < students[j+1].get_admin_no():
                 tmp = students[j]
                 students[j] = students[j+1]
                 students[j+1] = tmp
+                swapmade = True
+        if swapmade == False:
+            break
+        print(" ")
+        print(" ")
+        print("Pass ",count)
+        count += 1
+        print("**********************************")
+        for student in students:
+            print("AdminNum: ", student.get_admin_no())
+
+    print("")
+    print("**************************************")
     print("Students sorted by Admin Number in descending order: ")
     display_students(students)
 
 def insertion_sort_pem_group(students):
     n = len(students)
+    count = 1
     for i in range(1, n):
         value = students[i]
         pos = i
@@ -94,8 +93,34 @@ def insertion_sort_pem_group(students):
     print("Students sorted by PEM Group in ascending order")
     display_students(students)
 
+def populateData():
+    studList = []
+    newStudA = Student("2101252Y", "steve", "steve@mail.com",
+    "2021", "IT2101")
+    studList.append(newStudA)
+    newStudA = Student("2121613Y", "strange", "tony@mail.com",
+    "2021", "IT2103")
+    studList.append(newStudA)
+    newStudA = Student("2101122A", "peter", "peter@mail.com",
+    "2021", "IT2101")
+    studList.append(newStudA)
+    newStudA = Student("2121623Y", "tony", "tony@mail.com", "2021",
+    "IT2103")
+    studList.append(newStudA)
+    newStudA = Student("2121523Y", "banner", "banner@mail.com",
+    "2021", "IT2102")
+    studList.append(newStudA)
+    newStudA = Student("2121563H", "clark", "clark@mail.com",
+    "2021", "IT2102")
+    studList.append(newStudA)
+    newStudA = Student("2111025C", "bruce", "bruce@mail.com",
+    "2021", "IT2102")
+    studList.append(newStudA)
+    print("Data populated!\n")
+    return studList
 
 def main():
+    students = []
     while True:
         print("")
         print("*******************************")
@@ -105,6 +130,7 @@ def main():
         print("3. Sort students by AdminNo in descending order.")
         print("4. Sort students by PEM Group in ascending order.")
         print("5. Exit.")
+        print("9. Populate data")
 
         choice = input("Enter your choice (1-5): ")
 
@@ -119,6 +145,8 @@ def main():
         elif choice == '5':
             print("Exiting the program.")
             break
+        elif choice == '9':
+            students = populateData()
         else:
             print("Invalid choice. Please enter a number between 1 and 5.")
 
